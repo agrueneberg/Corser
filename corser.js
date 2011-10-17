5bb4cdb446d4fa66e8f9688786afefeddc653e08
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2011-10-17 11:37:04 -0500

    Return a JSON object instead of plain text.

--- corser.js
+++ corser.js
@@ -1,7 +1,9 @@
 var http = require('http');
 http.createServer(function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World: ' + req.method + '\n');
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify({
+        message: 'Answered ' + req.method + ' request from ' + req.headers.host
+    }));
 }).listen(80, '0.0.0.0');
 console.log('Server running at http://127.0.0.1:1337/');
