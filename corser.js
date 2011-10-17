4ba711a3aee3779e6d0d81d7faa191443c683c52
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2011-10-17 11:57:55 -0500

    Add PUT and DELETE to Access-Control-Allow-Methods.

--- corser.js
+++ corser.js
@@ -1,6 +1,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.writeHead(200, {'Content-Type': 'application/json'});
     res.end(JSON.stringify({
         message: req.headers.host + ' answered a ' + req.method + ' request.'
