074cfa0049c47457a1e88c5427bc88e25e02c488
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2011-10-17 11:55:18 -0500

    The Host header refers to the host and port number of the resource being requested, not the other way around.

--- corser.js
+++ corser.js
@@ -3,7 +3,7 @@ http.createServer(function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.writeHead(200, {'Content-Type': 'application/json'});
     res.end(JSON.stringify({
-        message: 'Answered ' + req.method + ' request from ' + req.headers.host
+        message: req.headers.host + ' answered a ' + req.method + ' request.'
     }));
 }).listen(80, '0.0.0.0');
 console.log('Server running at http://127.0.0.1:1337/');
