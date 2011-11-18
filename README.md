2de95dfe617cd3ac2d0ebc4f435871b6ad330784
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2011-11-18 15:25:47 -0600

    Added reverse proxy capabilities.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Corser
 =======
 
-Corser is a [CORS](http://www.w3.org/TR/cors/) enabled HTTP server running on [node.js](http://nodejs.org/).
+Corser is a [CORS](http://www.w3.org/TR/cors/)-enabled HTTP reverse proxy running on [Node](http://nodejs.org/).
--- corser.js
+++ corser.js
@@ -1,12 +1,41 @@
-var http, port;
+var argv, http, url;
+argv = require('optimist').default('port', 80).alias('p', 'port').argv;
 http = require('http');
-port = 80;
+url = require('url');
 http.createServer(function (req, res) {
+    var parsedUrl, proxyUrl;
+    // Enable Cross-Origin Resource Sharing (CORS).
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({
-        message: req.headers.host + ' answered a ' + req.method + ' request.'
-    }));
-}).listen(port);
-console.log('Server running on port ' + port + '.');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, HEAD, OPTIONS');
+    parsedUrl = url.parse(req.url, true);
+    if (parsedUrl.query.hasOwnProperty('url')) {
+        proxyUrl = url.parse(parsedUrl.query.url);
+        var proxyObject = {
+            host: proxyUrl.host,
+            port: proxyUrl.port || 80,
+            path: proxyUrl.path,
+            method: req.method
+        };
+        var proxyReq = http.request(proxyObject, function (proxyRes) {
+            var contentType;
+            contentType = proxyRes.headers['content-type'] || 'text/plain';
+            res.writeHead(200, contentType);
+            proxyRes.on('data', function (chunk) {
+                res.write(chunk);
+            });
+            proxyRes.on('end', function () {
+                res.end();
+            });
+        });
+        proxyReq.on('error', function (e) {
+            res.writeHead(500);
+            console.log(e);
+            res.end('Captain, we crashed: ' + e.code + '\n');
+        });
+        proxyReq.end();
+    } else {
+        res.writeHead(400);
+        res.end('"url", did you pass it?\n');
+    }
+}).listen(argv.port);
+console.log('Server running at http://127.0.0.1:' + argv.port + '/');
