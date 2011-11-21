732ba6278a2b9832ce8ff200aac8f774cb7f54a0
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2011-11-21 11:45:30 -0600

    Use " instead of '.

--- corser.js
+++ corser.js
@@ -1,41 +1,40 @@
 var argv, http, url;
-argv = require('optimist').default('port', 80).alias('p', 'port').argv;
-http = require('http');
-url = require('url');
+argv = require("optimist").default("port", 80).alias("p", "port").argv;
+http = require("http");
+url = require("url");
 http.createServer(function (req, res) {
     var parsedUrl, proxyUrl;
     // Enable Cross-Origin Resource Sharing (CORS).
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, HEAD, OPTIONS');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, HEAD, OPTIONS");
     parsedUrl = url.parse(req.url, true);
-    if (parsedUrl.query.hasOwnProperty('url')) {
+    if (parsedUrl.query.hasOwnProperty("url")) {
         proxyUrl = url.parse(parsedUrl.query.url);
         var proxyObject = {
-            host: proxyUrl.host,
-            port: proxyUrl.port || 80,
-            path: proxyUrl.path,
-            method: req.method
+            "host": proxyUrl.host,
+            "port": proxyUrl.port || 80,
+            "path": proxyUrl.path,
+            "method": req.method
         };
         var proxyReq = http.request(proxyObject, function (proxyRes) {
             var contentType;
-            contentType = proxyRes.headers['content-type'] || 'text/plain';
+            contentType = proxyRes.headers["content-type"] || "text/plain";
             res.writeHead(200, contentType);
-            proxyRes.on('data', function (chunk) {
+            proxyRes.on("data", function (chunk) {
                 res.write(chunk);
             });
-            proxyRes.on('end', function () {
+            proxyRes.on("end", function () {
                 res.end();
             });
         });
-        proxyReq.on('error', function (e) {
+        proxyReq.on("error", function (e) {
             res.writeHead(500);
-            console.log(e);
-            res.end('Captain, we crashed: ' + e.code + '\n');
+            res.end("Captain, we crashed: " + e.code + "\n");
         });
         proxyReq.end();
     } else {
         res.writeHead(400);
-        res.end('"url", did you pass it?\n');
+        res.end("\"url\", did you pass it?\n");
     }
 }).listen(argv.port);
-console.log('Server running at http://127.0.0.1:' + argv.port + '/');
+console.log("Server running at http://127.0.0.1:" + argv.port + "/");
